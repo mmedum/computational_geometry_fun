88e8d729511953f7ed537ec69ca063b1cdf07902
Mark Medum Bundgaard <mmedum@gmail.com>
2017-11-07 13:15:45 +0100

    graham_scan implementation and quick_hull should start to behave somewhat nicely

--- handin3/graham_scan.py
+++ handin3/graham_scan.py
@@ -12,6 +12,19 @@ class Point:
     def y(self):
         return self._y
 
+    def __str__(self):
+        return '{},{}'.format(self._x, self._y)
+
+
+def read_points(path):
+    points = []
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            coordinates = line.split(',')
+            points.append(Point(int(coordinates[0].strip()), int(coordinates[1].strip())))
+    points.sort(key=lambda p: p.x)
+    return points
+
 
 # Three points are a counter-clockwise turn if ccw > 0, clockwise if
 # ccw < 0, and collinear if ccw = 0 because ccw is a determinant that
@@ -20,9 +33,29 @@ def ccw(p1, p2, p3):
     return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x)
 
 
-def graham_scan():
-    print('IMPLEMENT')
+def graham_scan(points):
+    upper_hull = []
+    for p in points:
+        while len(upper_hull) > 1 and ccw(upper_hull[-2], upper_hull[-1], p) >= 0:
+            upper_hull.pop()
+        upper_hull.append(p)
+    lower_hull = []
+    for p in points:
+        while len(lower_hull) > 1 and ccw(lower_hull[-2], lower_hull[-1], p) <= 0:
+            lower_hull.pop()
+        lower_hull.append(p)
+    lower_hull.reverse()
+    upper_hull.extend(lower_hull[1:])
+    return upper_hull
+
+
+def output_convex_hull(convex_hull):
+    print('Graham Scan')
+    for c in convex_hull:
+        print(c)
 
 
 if __name__ == '__main__':
-    graham_scan()
+    points = read_points('points')
+    convex_hull = graham_scan(points)
+    output_convex_hull(convex_hull)
---
+++ handin3/quick_hull.py
@@ -0,0 +1,110 @@
+class Point:
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    def __str__(self):
+        return '{},{}'.format(self._x, self._y)
+
+
+def read_points(path):
+    points = []
+    with open(path, 'r') as file:
+        for line in file.readlines():
+            coordinates = line.split(',')
+            points.append(Point(int(coordinates[0].strip()), int(coordinates[1].strip())))
+    points.sort(key=lambda p: p.x)
+    return points
+
+
+def output_convex_hull(quick_hull):
+    for c in quick_hull:
+        print(c)
+
+
+# Three points are a counter-clockwise turn if ccw > 0, clockwise if
+# ccw < 0, and collinear if ccw = 0 because ccw is a determinant that
+# gives twice the signed  area of the triangle formed by p1, p2 and p3.
+def sidedness_test(p1, p2, p3):
+    return (p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x)
+
+
+def distance_point_to_line(p1, p2, p3):
+    return abs((p2.x - p1.x)*(p3.y - p1.y) - (p2.y - p1.y)*(p3.x - p1.x))
+
+
+def prune(line_segment, points):
+    pruned_points = []
+    for p in points:
+        if sidedness_test(line_segment[0], line_segment[1], p) >= 0:
+            pruned_points.append(p)
+    return pruned_points
+
+
+def find_point_with_largest_distance(line_segment, points):
+    max_distance = -1
+    max_point = None
+    for p in points:
+        current_distance = distance_point_to_line(line_segment[0], line_segment[1], p)
+        if current_distance > max_distance:
+            max_distance = current_distance
+            max_point = p
+    return max_point
+
+
+def rec_quick_hull(line_segment, points):
+    print('POINTS {}'.format(points))
+    if len(points) == 0 or points is None:
+        print('HERE {}'.format(output_convex_hull(points)))
+        return [line_segment[0], line_segment[1]]
+    largest_distance_point = find_point_with_largest_distance(line_segment, points)
+    upper_hull = []
+    upper_hull.append(largest_distance_point)
+
+    left_points = []
+    right_points = []
+    for p in points:
+        if p.x <= largest_distance_point.x:
+            left_points.append(p)
+        else:
+            right_points.append(p)
+
+    print('left {}'.format(output_convex_hull(left_points)))
+    print('Right {}'.format(output_convex_hull(right_points)))
+
+    left_line_segment = (line_segment[0], largest_distance_point)
+    left_pruned_points = prune(left_line_segment, left_points)
+    print('Left Prune {}'.format(output_convex_hull(left_pruned_points)))
+
+    right_line_segment = (largest_distance_point, line_segment[1])
+    right_pruned_points = prune(right_line_segment, right_points)
+    print('Right Prune {}'.format(output_convex_hull(right_pruned_points)))
+
+    upper_hull.extend(rec_quick_hull(left_line_segment, left_pruned_points))
+    print('Upper {}'.format(output_convex_hull(upper_hull)))
+    upper_hull.extend(rec_quick_hull(right_line_segment, right_pruned_points))
+    return upper_hull
+
+
+def quick_hull(points):
+    points.sort(key=lambda p: p.x)
+    smallest = points[0]
+    largest = points[-1]
+    line_segment = (smallest, largest)
+    pruned_points = prune(line_segment, points[1:-2])
+    return rec_quick_hull(line_segment, pruned_points)
+
+
+if __name__ == '__main__':
+    points = read_points('points')
+    upper_hull = quick_hull(points)
+#    output_convex_hull(upper_hull)

c7cd4efb5b140fa359dccc75e9430f2c2ff0a477
Mark Medum Bundgaard <mmedum@gmail.com>
2017-11-21 15:44:36 +0100

    this is a temp solution that gives NoneType mistake and misses points, but when it works it works, thats the most important thing."

--- handin3/marriage_before_conquest.py
+++ handin3/marriage_before_conquest.py
@@ -1,33 +1,140 @@
-from common import read_points, output_convex_hull, linprog
+from common import point
 import random
 
 
+def linear_prog_1d(constraints, obj_function):
+    right = float('inf')
+    left = float('-inf')
+
+    for constraint in constraints:
+        if constraint.x == 0:
+            if constraint.y > 0:
+                return None
+        else:
+            ratio = constraint.y / constraint.x
+            if constraint.x > 0:
+                left = max(left, ratio)
+            else:
+                if constraint.x < 0:
+                    right = min(right, ratio)
+                else:
+                    if constraint.y > 0:
+                        return None
+
+    if left > right:
+        return None
+
+    if obj_function >= 0:
+        return left
+    else:
+        return right
+
+
+def calculate_a_and_b(p, q):
+    print('P', p)
+    print('Q', q)
+    a = (q.y - p.y) / (q.x - p.x)
+    b = p.y - a * p.x
+    return a, b
+
+
+def satisfy(a, b, constraint):
+    return constraint.y <= a * constraint.x + b
+
+
+def linear_prog(points_left, points_right, median):
+    a, b = calculate_a_and_b(points_left[0], points_right[0])
+
+    if len(points_left) == 1 and len(points_right) == 1:
+        return a, b
+
+    constraints = []
+    points_left = points_left[1:]
+    points_right = points_right[1:]
+    points_left.extend(points_right)
+    for p in points_left:
+        constraints.append(p)
+        if not satisfy(a, b, p):
+            constraints_in_1d = []
+            for c in constraints:
+                constraints_in_1d.append(point.Point(c.x - p.x, c.y - p.y))
+            obj_function = median.x - p.x
+            a = linear_prog_1d(constraints_in_1d, obj_function)
+            b = p.y - a * p.x
+    return a, b
+
+
+def find_min_point(a, b, points):
+    min_dis = float('inf')
+    min_point = points[0]
+    for p in points:
+        dis = (a * p.x + b) - p.y
+        if dis < min_dis:
+            min_point = p
+            min_dis = dis
+    return min_point
+
+
+def prune_left(line_segment, points):
+    survived_points = []
+    for p in points:
+        if p.x < line_segment[0].x:
+            survived_points.append(p)
+    return survived_points
+
+
+def prune_right(line_segment, points):
+    survived_points = []
+    for p in points:
+        if p.x > line_segment[1].x:
+            survived_points.append(p)
+    return survived_points
+
+
 def marriage_before_conquest_v1(points):
+    hull = []
     # Step 1
-    upper_hull = []
+    if len(points) <= 1:
+        return []
+    if len(points) == 2:
+        line_segment = [points[0], points[1]]
+        hull.append(line_segment)
+        return hull
     sample_elements = random.sample(points, 3)
     sample_elements.sort(key=lambda p: p.x)
-    median_element = sample_elements[2]
-    print('Median element {}'.format(median_element))
+    median = sample_elements[1]
 
     points_left = []
     points_right = []
     for p in points:
-        if p.x < median_element.x:
+        if p is median:
+            points_left.append(p)
+        elif p.x < median.x:
             points_left.append(p)
         else:
             points_right.append(p)
 
-    linprog.lin_prog_2d(points, median_element)
+    # TODO shuffle the shit
+    a, b = linear_prog(points_left, points_right, median)
 
-    upper_hull.extend(marriage_before_conquest_v1(points_left))
-    upper_hull.extend(marriage_before_conquest_v1(points_right))
-    print(upper_hull)
+    min_left_point = find_min_point(a, b, points_left)
+    min_right_point = find_min_point(a, b, points_right)
+    line_segment = [min_left_point, min_right_point]
 
-    return upper_hull
+    hull.append(line_segment)
+    points_left = prune_left(line_segment, points_left)
+    points_left.append(min_left_point)
+
+    points_right = prune_right(line_segment, points_right)
+    points_right.append(min_right_point)
+
+    hull.extend(marriage_before_conquest_v1(points_left))
+    hull.extend(marriage_before_conquest_v1(points_right))
+
+    return hull
 
 
 if __name__ == '__main__':
-    points = read_points('points')
+    points = point.read_points('points')
     convex_hull = marriage_before_conquest_v1(points)
-    output_convex_hull('gift_wrap', convex_hull)
+    print(convex_hull)
